529a414f57b3eaaa6d83c6607f07fb8f89341403
Saidbek <saidbek1411@gmail.com>
2025-12-30 11:43:34 +0500

    created model , migation, seeder, ResultTest

---
+++ database/seeders/ResultTestSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ResultTestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
